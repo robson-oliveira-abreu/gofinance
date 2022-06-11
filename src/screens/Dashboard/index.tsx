3ed41c164fc5269729ff779ed67afcd300165743
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-06-11 00:53:05 -0300

    update styled-components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+
 import { HighlightCard } from "../../Components/HighlightCard";
+import { TransactionCard } from "../../Components/TransactionCard";
 
 import {
     Container,
@@ -11,49 +14,94 @@ import {
     UserName,
     UserWrapper,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList,
 
 } from './styles'
 
 export function Dashboard() {
-    return (
-        <Container>
-            <Header>
-                <UserWrapper>
-                    <UserInfo>
-                        <Photo
-                            source={{ uri: 'https://avatars.githubusercontent.com/u/77408728?v=4' }}
-                        />
-                        <User>
-                            <UserGreeting>Olá</UserGreeting>
-                            <UserName>Robson</UserName>
-                        </User>
-                    </UserInfo>
+    const data = [{
+        title: 'Desenvolvimento de site',
+        amount: 'R$ 12.000,00',
+        category: {
+            name: 'Vendas',
+            icon: 'dollar-sign'
+        },
+        date: '13/04/2020'
+    },
+    {
+        title: 'Desenvolvimento de site',
+        amount: 'R$ 12.000,00',
+        category: {
+            name: 'Vendas',
+            icon: 'dollar-sign'
+        },
+        date: '13/04/2020'
+    },
+    {
+        title: 'Desenvolvimento de site',
+        amount: 'R$ 12.000,00',
+        category: {
+            name: 'Vendas',
+            icon: 'dollar-sign'
+        },
+        date: '13/04/2020'
+    }];
 
-                    <Icon name='power' />
-                </UserWrapper>
-            </Header>
+return (
+    <Container>
+        <Header>
+            <UserWrapper>
+                <UserInfo>
+                    <Photo
+                        source={{ uri: 'https://avatars.githubusercontent.com/u/77408728?v=4' }}
+                    />
+                    <User>
+                        <UserGreeting>Olá</UserGreeting>
+                        <UserName>Robson</UserName>
+                    </User>
+                </UserInfo>
 
-            <HighlightCards>
-                <HighlightCard
-                    type='up'
-                    title='Entradas'
-                    amount='R$ 17.400,00'
-                    lastTransaction='Última entrada dia 13 de abril'
-                />
-                <HighlightCard
-                    type='down'
-                    title='Saídas'
-                    amount='R$ 1.259,00'
-                    lastTransaction='Última saída dia 03 de abril'
-                />
-                <HighlightCard
-                    type='total'
-                    title='Total'
-                    amount='R$ 16.141,00'
-                    lastTransaction='01 á 16 de abril'
-                />
-            </HighlightCards>
-        </Container>
-    )
+                <Icon name='power' />
+            </UserWrapper>
+        </Header>
+
+        <HighlightCards>
+            <HighlightCard
+                type='up'
+                title='Entradas'
+                amount='R$ 17.400,00'
+                lastTransaction='Última entrada dia 13 de abril'
+            />
+            <HighlightCard
+                type='down'
+                title='Saídas'
+                amount='R$ 1.259,00'
+                lastTransaction='Última saída dia 03 de abril'
+            />
+            <HighlightCard
+                type='total'
+                title='Total'
+                amount='R$ 16.141,00'
+                lastTransaction='01 á 16 de abril'
+            />
+        </HighlightCards>
+
+        <Transactions>
+            <Title>Listagem</Title>
+            <TransactionList 
+                data={data}
+                renderItem={({item}) => <TransactionCard data={item}/>}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{
+                    paddingBottom: getBottomSpace()
+                }}
+            />
+
+            
+        </Transactions>
+    </Container>
+)
 }
